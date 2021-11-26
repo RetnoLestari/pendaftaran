8743902b110315778aa9db8a8992f2d0a0ae019e
Retno Lestari <92641950+RetnoLestari@users.noreply.github.com>
2021-11-26 12:22:05 +0700

    Add files via upload

---
+++ tuming1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	string a, b, c, d;
+	
+	//Input
+	cout<<"";
+	getline(cin,a);
+	cout<<"";
+	getline(cin,b);
+	cout<<"";
+	getline(cin,c);
+	cout<<"";
+	getline(cin,d);
+	
+	//Output
+	cout<<"Nama Pendaftar    : "<<a<<endl;
+	cout<<"Nomor HP          : "<<b<<endl;
+	cout<<"Alamat Pendaftar  : "<<c<<endl;
+	cout<<"Alasan Mendaftar  : "<<d<<endl;
+	
+	return 0;
+}
